4c1b7e069fd65cd34d31187901767a70a8b3ebb0
ilteber <ayvaci.ilteber@gmail.com>
2020-07-18 22:03:47 +0200

    - Account Setup added (further styling to be done in the future)

--- src/components/EventFormPage.js
+++ src/components/EventFormPage.js
@@ -16,6 +16,13 @@ import Paper from "@material-ui/core/Paper";
 import Chip from "@material-ui/core/Chip";
 import Typography from "@material-ui/core/Typography";
 import SaveIcon from "@material-ui/icons/Save";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import CardMedia from "@material-ui/core/CardMedia";
+import {PhotoCamera} from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
 
@@ -49,14 +56,21 @@ const useStyles = makeStyles(theme => ({
         '&:hover': {
             backgroundColor: '#e13434',
         }
-    }
+    },
+    button: {
+        margin: theme.spacing(1),
+    },
+    imageInput:{
+        display: 'none',
+    },
+
 }));
 
 function EventFormPage(props) {
 
     const classes = useStyles();
 
-    let initStateName,initStateDateStart,initStateDateEnd, initStateDesc, initStateLoc, initStateCap, initStatePrice;
+    let initStateName,initStateDateStart,initStateDateEnd, initStateDesc, initStateLoc, initStateCap, initStatePrice, initSelectedImage;
     let initStateTags = [];
 
     if(props.event != undefined){
@@ -68,6 +82,7 @@ function EventFormPage(props) {
         initStateDesc = props.event.description;
         initStateLoc = props.event.location;
         initStatePrice = props.event.price;
+        initSelectedImage = props.event.image;
     }
 
     const [eventName, setEventName] = useState(initStateName);
@@ -78,6 +93,8 @@ function EventFormPage(props) {
     const [location, setLocation] = useState(initStateLoc);
     const [description, setDescription] = useState(initStateDesc);
     const [selectedTags, setSelectedTags]  = React.useState(initStateTags);
+    const [selectedImage, setSelectedImage]  = React.useState(initSelectedImage);
+    const [selectedImageBinary, setSelectedImageBinary]  = React.useState(initSelectedImage);
     const [tags, setTags]  = React.useState(
         [
             {key: 0, data: 'Tennis'},
@@ -145,9 +162,21 @@ function EventFormPage(props) {
         event.location = location;
         event.description = description;
         event.tags = selectedTags;
+        event.image = selectedImageBinary;
+
         props.onSubmit(event);
     }
 
+    function handleImageUpload (event) {
+        let file = event.target.files[0];
+        setSelectedImageBinary(file);
+        const reader = new FileReader();
+        let url = reader.readAsDataURL(file);
+        reader.onloadend = function(e) {
+            setSelectedImage(reader.result);
+        };
+
+    }
     return (
         <div className={classes.root}>
             <OrganizerHeader/>
@@ -273,18 +302,60 @@ function EventFormPage(props) {
                                 })}
                             </Box>
                         </Box>
-                            <Paper >
-                                {tags.map((data) => {
-                                    return (
-                                        <Chip key={data.key}
-                                              label={data.data}
-                                              clickable
-                                              onClick={() => handleClickTag(data)}
-                                              className={classes.chip}
-                                        />
-                                    );
-                                })}
-                            </Paper>
+                        <Paper >
+                            {tags.map((data) => {
+                                return (
+                                    <Chip key={data.key}
+                                          label={data.data}
+                                          clickable
+                                          onClick={() => handleClickTag(data)}
+                                          className={classes.chip}
+                                    />
+                                );
+                            })}
+                        </Paper>
+                        <Box display="flex" flexDirection="row">
+                            <input
+                                accept="image/*"
+                                className={classes.imageInput}
+                                id="image-button-file"
+                                type="file"
+                                onChange={(event) => handleImageUpload(event)}
+                            />
+                            <label htmlFor="image-button-file">
+                                <Button
+                                    variant="contained"
+                                    color="default"
+                                    className={classes.button}
+                                    startIcon={<PhotoCamera/>}
+                                    component="span"
+                                >
+                                    Upload Thumbnail image
+                                </Button>
+                            </label>
+                            {selectedImage !== undefined ?
+
+                                <Box>
+                                    <img
+                                        width="50%"
+                                        // className={classes.media}
+                                        src={selectedImage}
+                                    />
+                                </Box>
+
+                                :
+                                <div></div>
+                            }
+                            {/*<React.Fragment>*/}
+                            {/*    <CardActionArea onClick={this.imageResetHandler}>*/}
+                            {/*        <img*/}
+                            {/*            width="100%"*/}
+                            {/*            className={classes.media}*/}
+                            {/*            src={this.state.selectedFile}*/}
+                            {/*        />*/}
+                            {/*    </CardActionArea>*/}
+                            {/*</React.Fragment>*/}
+                        </Box>
                     <Box display="flex" justifyContent="flex-end" m={1} p={1}>
                     <Button id="submit" type="submit" startIcon={<SaveIcon />} className={classes.saveButton}
                             disabled={eventName === undefined || eventName === '' || eventDateStart === undefined || eventDateStart === ''}
