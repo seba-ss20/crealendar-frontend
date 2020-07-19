c13ce9248ab169bca4bf4b5c6ce542f7e831391a
ga34ram_ <ga34ram@mytum.de>
2020-07-19 16:06:40 +0200

    adding listitemicon to imports

--- src/components/EventPage/AddFriendsList.js
+++ src/components/EventPage/AddFriendsList.js
@@ -20,6 +20,7 @@ import Avatar from '@material-ui/core/Avatar';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import Checkbox from '@material-ui/core/Checkbox';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import { FixedSizeList } from 'react-window';
